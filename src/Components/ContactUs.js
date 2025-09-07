b88ebf15d488d21dd9f5f20363eadece80bbb051
36294966 <erastusngamau8@gmail.com>
2025-09-08 00:33:06 +0200

    initial commit

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -54,9 +54,7 @@ function Contact() {
       </div>
 
       {/* Footer section */}
-      <footer className="bg-blue-600 text-white py-4 text-center">
-        <p>&copy; 2025 Your Company Name</p>
-      </footer>
+     
     </div>
   );
 }
